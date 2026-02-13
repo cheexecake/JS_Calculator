da1a35c1f3b69fb96aba1cecfca71f30a82a7005
cheexecake <thandon57@gmail.com>
2026-02-13 01:28:39 -0500

    Operator functions added, Add buttons for interactivity

---
+++ calculator.js
@@ -0,0 +1,23 @@
+
+
+function addition(n1,n2){
+    return n1+n2;
+}
+
+function subtraction(n1,n2){
+    return n1-n2;
+}
+
+function multiplication(n1,n2){
+    return n1*n2;
+}
+
+function division(n1,n2){
+    return n1/n2;
+}
+
+
+subtraction(3,4);
+
+
+console.log(subtraction(3,4))
